e18bff3e82f19fa107cdc82d5f7463b5ca581750
Dominique Michel <dominique.c.michel@gmail.com>
2024-07-25 18:55:17 +0200

    FontSelector: workaround some fvwm limitation when the Noto fonts are installed by removing their vatiants from  the font list.

--- fvwm/scripts/FontSelector/findindex.sh
+++ fvwm/scripts/FontSelector/findindex.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Find the index of a font
+# Syntax:	findindex.sh "<font name>"
 
 index=1
 
-fc-list : family 2>/dev/null | sort | while read line
+"${FVWM_SYSTEMDIR}"/scripts/FontSelector/getlistindex | while read -r line
 do
     if [[ "${line}" == "$1" ]]; then
 	echo "${index}"
